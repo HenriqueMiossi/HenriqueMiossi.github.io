c895650ab58837779462ba1b026775e11014d379
HenriqueMiossi <henriquemiossi@gmail.com>
2022-02-17 16:45:13 -0300

    Fix next export issue

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const path = require('path')
+const withOptimizedImages = require('next-optimized-images');
+module.exports = withOptimizedImages({
+  /* config for next-optimized-images */
+  
+  images: {
+    loader: 'custom',
+    disableStaticImages: true
+  },
 
-module.exports = nextConfig
+  // sassOptions: {
+  //   includePaths: [path.join(__dirname, 'src/assets/scss')],
+  // },
+
+});
